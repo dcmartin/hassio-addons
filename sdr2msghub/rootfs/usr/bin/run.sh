c821ff6021e3f9a44c5ddfbd74137f431f4eefcc
dcmartin <github@dcmartin.com>
2018-11-01 13:29:48 -0700

    Hmm...

--- sdr2msghub/rootfs/usr/bin/run.sh
+++ sdr2msghub/rootfs/usr/bin/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/with-contenv bash
 # ==============================================================================
 set -o nounset  # Exit script on use of an undefined variable
-# set -o pipefail # Return exit status of the last command in the pipe that failed
+set -o pipefail # Return exit status of the last command in the pipe that failed
 # set -o errtrace # Exit on error inside any functions or sub-shells
 # set -o errexit  # Exit script when a command exits with non-zero status
 
