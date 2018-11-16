731420abd8f5d36456a67613ae952eac6b1188ef
dcmartin <github@dcmartin.com>
2018-11-16 10:27:59 -0800

    Changed output to stderr and exits on failure to 1; added stdout of JSON with configuration

--- horizon/hzn-setup.sh
+++ horizon/hzn-setup.sh
@@ -3,21 +3,21 @@
 ## BASE
 
 if [ "${VENDOR}" == "apple" ] || [ "${OSTYPE}" == "darwin" ]; then
-  echo "You're on a Macintosh; download from https://github.com/open-horizon/anax/releases; exiting"
-  exit
+  echo "You're on a Macintosh; download from https://github.com/open-horizon/anax/releases; exiting" >&2
+  exit 1
 fi
 
 if [ $(whoami) != "root" ]; then
-  echo "!!! ERROR: Please run as root, e.g. sudo $0 $*"
-  exit
+  echo "!!! ERROR: Please run as root, e.g. sudo $0 $*" >&2
+  exit 1
 fi
 
 # Specify the hardware architecture of this Edge Node
 
 ARCH=$(arch)
 if [ -z "${ARCH}" ]; then
-  echo "!!! ERROR: No architecture detected; exiting"
-  exit
+  echo "!!! ERROR: No architecture detected; exiting" >&2
+  exit 1
 fi
 
 if [ "${ARCH}" == "aarch64" ]; then
@@ -27,35 +27,35 @@ elif [ "${ARCH}" == "x86_64" ]; then
 elif [ "${ARCH}" == "armv7l" ]; then
   ARCH=$(dpkg --print-architecture)
 else
-  echo "Cannot automagically identify architecture (${ARCH}); options are: arm, arm64, amd64, ppc64el"
+  echo "Cannot automagically identify architecture (${ARCH}); options are: arm, arm64, amd64, ppc64el" >&2
   read -p 'Architecture: ' ARCH
   if [ -n "${ARCH}" ]; then
-    echo "+++ INFO: Specified ${ARCH}"
+    echo "+++ INFO: Specified ${ARCH}" >&2
   else
-    echo "!!! ERROR: No architecture defined; exiting"
-    exit
+    echo "!!! ERROR: No architecture defined; exiting" >&2
+    exit 1
   fi
 fi
 
-echo "+++ INFO: Using architecture: ${ARCH}"
+echo "+++ INFO: Using architecture: ${ARCH}" >&2
 
 # check apt
 CMD=$(command -v apt)
 if [ -z "${CMD}" ]; then
-  echo "!!! ERROR: No apt(1) package installation; are you using Alpine; exiting"
-  exit
+  echo "!!! ERROR: No apt(1) package installation; are you using Alpine; exiting" >&2
+  exit 1
 fi
 
 # warn about DNS
 if [ -e "/run/systemd/resolve/resolv.conf" ]; then
-  echo "*** WARN: Check your /etc/resolv.conf for link to /run/systemd/resolve/resolv.conf under Ubuntu 18.04"
+  echo "*** WARN: Check your /etc/resolv.conf for link to /run/systemd/resolve/resolv.conf under Ubuntu 18.04" >&2
 fi
 
 # install pre-requisites
 for CMD in jq curl ssh kafkacat; do
   C=$(command -v $CMD)
   if [ -z "${C}" ]; then
-    echo "+++ INFO: Installing ${CMD}"
+    echo "+++ INFO: Installing ${CMD}" >&2
     apt-get install -y ${CMD}
   fi
 done
@@ -63,7 +63,7 @@ done
 # check docker
 CMD=$(command -v docker)
 if [ -z "${CMD}" ]; then
-  echo "*** WARN: Installing docker"
+  echo "*** WARN: Installing docker" >&2
   curl -fsSL "get.docker.com" | sh
 fi
 
@@ -71,54 +71,55 @@ fi
 ### HORIZON
 ###
 
+if [ ! -n "${APT_REPO}" ]; then
+  APT_REPO=testing
+  echo "*** WARN: Using default APT_REPO = ${APT_REPO}" >&2
+fi
+
 # CLI
 CMD=$(command -v hzn)
 if [ ! -z "${CMD}" ]; then
-  echo "*** WARN: Open Horizon already installed as ${CMD}; skipping"
+  echo "*** WARN: Open Horizon already installed as ${CMD}; skipping" >&2
 else
-  if [ ! -n "${APT_REPO}" ]; then
-    APT_REPO=testing
-    echo "*** WARN: Using default APT_REPO = ${APT_REPO}"
-  fi
   if [ ! -n "${APT_LIST}" ]; then
     APT_LIST=/etc/apt/sources.list.d/bluehorizon.list
-    echo "*** WARN: Using default APT_LIST = ${APT_LIST}"
+    echo "*** WARN: Using default APT_LIST = ${APT_LIST}" >&2
   fi
   if [ ! -n "${PUBLICKEY_URL}" ]; then
     PUBLICKEY_URL=http://pkg.bluehorizon.network/bluehorizon.network-public.key
-    echo "*** WARN: Using default PUBLICKEY_URL = ${PUBLICKEY_URL}"
+    echo "*** WARN: Using default PUBLICKEY_URL = ${PUBLICKEY_URL}" >&2
   fi
   if [ -s "${APT_LIST}" ]; then
-    echo "*** WARN: Existing Open Horizon ${APT_LIST}; deleting"
+    echo "*** WARN: Existing Open Horizon ${APT_LIST}; deleting" >&2
     rm -f "${APT_LIST}"
   fi
   # get public key and install
-  echo "+++ INFO: Adding key for Open Horizon from ${PUBLICKEY_URL}"
+  echo "+++ INFO: Adding key for Open Horizon from ${PUBLICKEY_URL}" >&2
   curl -fsSL "${PUBLICKEY_URL}" | apt-key add -
-  echo "+++ INFO: Configuring Open Horizon repository ${APT_REPO} for ${ARCH}"
+  echo "+++ INFO: Configuring Open Horizon repository ${APT_REPO} for ${ARCH}" >&2
   # create repository entry 
   echo "deb [arch=${ARCH}] http://pkg.bluehorizon.network/linux/ubuntu xenial-${APT_REPO} main" >> "${APT_LIST}"
   echo "deb-src [arch=${ARCH}] http://pkg.bluehorizon.network/linux/ubuntu xenial-${APT_REPO} main" >> "${APT_LIST}"
-  echo "+++ INFO: Updating apt(1)"
+  echo "+++ INFO: Updating apt(1)" >&2
   apt-get update -y
-  echo "+++ INFO: Installing Open Horizon"
+  echo "+++ INFO: Installing Open Horizon" >&2
   apt-get install -y horizon bluehorizon
   # confirm installation
   if [ -z $(command -v hzn) ]; then
-    echo "!!! ERROR: Failed to install horizon; exiting"
-    exit
+    echo "!!! ERROR: Failed to install horizon; exiting" >&2
+    exit 1
   fi
 fi
 
 # LOGGING
 if [ -z "${LOG_CONF}" ]; then
   LOG_CONF=/etc/rsyslog.d/10-horizon-docker.conf
-  echo "*** WARN: Using default LOG_CONF = ${LOG_CONF}"
+  echo "*** WARN: Using default LOG_CONF = ${LOG_CONF}" >&2
 fi
 if [ -s "${LOG_CONF}" ]; then
-  echo "*** WARN: Existing logging configuration: ${LOG_CONF}; skipping"
+  echo "*** WARN: Existing logging configuration: ${LOG_CONF}; skipping" >&2
 else
-  echo "+++ INFO: Configuring logging: ${LOG_CONF}"
+  echo "+++ INFO: Configuring logging: ${LOG_CONF}" >&2
   rm -f "${LOG_CONF}"
   echo '$template DynamicWorkloadFile,"/var/log/workload/%syslogtag:R,ERE,1,DFLT:.*workload-([^\[]+)--end%.log"' >> "${LOG_CONF}"
   echo '' >> "${LOG_CONF}"
@@ -128,16 +129,18 @@ else
   echo '& stop' >> "${LOG_CONF}"
   echo ':syslogtag, startswith, "docker" -/var/log/docker.log' >> "${LOG_CONF}"
   echo '& stop' >> "${LOG_CONF}"
-  echo "+++ INFO: Restarting rsyslog(8)"
+  echo "+++ INFO: Restarting rsyslog(8)" >&2
   service rsyslog restart
 fi
 
 # SERVICE ACTIVATION
 if [ $(systemctl is-active horizon.service) != "active" ]; then
-  echo "+++ INFO: The horizon.service is not active; starting"
+  echo "+++ INFO: The horizon.service is not active; starting" >&2
   systemctl start horizon.service
 else
-  echo "*** WARN: The horizon.service is already active"
+  echo "*** WARN: The horizon.service is already active" >&2
 fi
 
-exit
+echo '{"repository":"'$APT_REPO'","version":"'`hzn version`'"}'
+
+exit 0
