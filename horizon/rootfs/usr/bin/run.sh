bbf88ab4209403a97e421c0dda5230663dc6c80e
David C. Martin <dcmartin@us.ibm.com>
2018-12-01 13:21:17 -0800

    dev

--- horizon/rootfs/usr/bin/run.sh
+++ horizon/rootfs/usr/bin/run.sh
@@ -338,12 +338,25 @@ main() {
   chmod 755 "${EXECPATH}"
   LOG_FILE="${CONFIG_PATH%/*}/${SCRIPT}.$(date +%s).log"
 
+  # check for configuration file
+  if [[ ! -s "${HORIZON_CONFIG_FILE}" ]]; then
+    hass.log.fatal "Configuration file not found: ${HORIZON_CONFIG_FILE}"
+    hass.die
+  fi
+  # check for executable
+  if [[ ! -s "${EXECPATH}" ]]; then
+    hass.log.fatal "Executable file not found: ${EXECPATH}"
+    hass.die
+  else
+    hass.log.debug "Executable permissions:" $(ls -al "${EXECPATH}")
+  fi
+
   while [[ NODE=$(hzn node list) ]]; do
     hass.log.debug "Node state: " $(echo "${NODE}" | jq '.configstate.state') "; workloads:" $(hzn agreement list | jq -r '.[]|.workload_to_run.url')
 
+    ## EVALUATE
     hass.log.info $(date) "${EXECPATH} on ${HORIZON_CONFIG_FILE} for ${HOST_LAN}; logging to ${LOG_FILE}"
-    eval "${EXECPATH}" "${HORIZON_CONFIG_FILE}" "${HOST_LAN}" &> "${LOG_FILE}"
-    hass.log.debug $(date) "${EXECPATH} returned ${STATUS}"
+    bash -- "${EXECPATH}" "${HORIZON_CONFIG_FILE}" "${HOST_LAN}" &> "${LOG_FILE}"
 
     # update/create configuration
     URL="${CLOUDANT_URL}/{${HORIZON_CONFIG_DB}/${HORIZON_CONFIG_NAME}"
