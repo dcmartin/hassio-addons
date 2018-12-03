eb277a7bd30eb5ee2cd93a62d2dd82a818a2c034
dcmartin <github@dcmartin.com>
2018-12-03 13:44:04 -0800

    dev

--- horizon/rootfs/usr/bin/run.sh
+++ horizon/rootfs/usr/bin/run.sh
@@ -323,8 +323,8 @@ main() {
     # parameters from addon options
     APACHE_ADMIN="${HORIZON_ORGANIZATION}"
     # APACHE_HOST="${HORIZON_DEVICE_NAME}" # ="hassio/addon_cb7b3237_horizon"
-    # APACHE_HOST="${HOST_IPADDR}"
-    APACHE_HOST="hassio/addon_cb7b3237_horizon"
+    APACHE_HOST="${HOST_IPADDR}"
+    # APACHE_HOST="hassio/addon_cb7b3237_horizon"
     # edit defaults
     sed -i 's|^Listen \(.*\)|Listen '${APACHE_PORT}'|' "${APACHE_CONF}"
     sed -i 's|^ServerName \(.*\)|ServerName '"${APACHE_HOST}:${APACHE_PORT}"'|' "${APACHE_CONF}"
