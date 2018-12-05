e531e3124d2589df8bc5a57924e9a25470e23bf6
dcmartin <github@dcmartin.com>
2018-12-05 08:10:41 -0800

    Broken up

--- sdr2msghub/rootfs/usr/bin/run.sh
+++ sdr2msghub/rootfs/usr/bin/run.sh
@@ -297,7 +297,16 @@ JQ='{"date":.ts?,"name":.devID?,"frequency":.freq?,"value":.expectedValue?,"long
 while [[ "${LISTEN_MODE}" != "false" ]]; do
   hass.log.info "Starting listen loop; routing ${KAFKA_TOPIC} to ${MQTT_TOPIC} at host ${MQTT_HOST} on port ${MQTT_PORT}"
   # wait on kafkacat death
-  kafkacat -u -C -q -o end -f "%s\n" -b $KAFKA_BROKER_URL -X "security.protocol=sasl_ssl" -X "sasl.mechanisms=PLAIN" -X "sasl.username=${KAFKA_API_KEY:0:16}" -X "sasl.password=${KAFKA_API_KEY:16}" -t "$KAFKA_TOPIC" | while read -r; do
+  kafkacat \
+    -u -C -q -o end \
+    -f "%s\n" \
+    -b $KAFKA_BROKER_URL \
+    -X "security.protocol=sasl_ssl" \
+    -X "sasl.mechanisms=PLAIN" \
+    -X "sasl.username=${KAFKA_API_KEY:0:16}" \
+    -X "sasl.password=${KAFKA_API_KEY:16}" \
+    -t "$KAFKA_TOPIC" \
+  | while read -r; do
     if [ -n "${REPLY}" ]; then
       PAYLOAD=$(echo "${REPLY}" | jq -c "${JQ}")
       hass.log.debug "RECEIVED: " $(echo "${PAYLOAD}" | jq -c '.audio="redacted"')
