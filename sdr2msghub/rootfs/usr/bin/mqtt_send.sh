ff67cab3640869f6f65f272aa5bf655d92e39287
dcmartin <github@dcmartin.com>
2018-12-06 14:03:05 -0800

    dev

--- sdr2msghub/rootfs/usr/bin/mqtt_send.sh
+++ sdr2msghub/rootfs/usr/bin/mqtt_send.sh
@@ -5,13 +5,19 @@ if [ -z "${ADDON_CONFIG_FILE:-}" ] || [ ! -s "${ADDON_CONFIG_FILE:-}" ]; then
   exit 1
 fi
 
+# identity of node
+MQTT_IDENTITY=$(jq -r ".horizon.device" "${ADDON_CONFIG_FILE}")
+if [ -z "${MQTT_IDENTITY}" ]; then
+  MQTT_IDENTITY=$(hostname)
+fi
+
 # find configuration
 MQTT_HOST=$(jq -r ".mqtt.host" "${ADDON_CONFIG_FILE}")
 MQTT_PORT=$(jq -r ".mqtt.port" "${ADDON_CONFIG_FILE}")
 MQTT_TOPIC=$(jq -r ".mqtt.topic" "${ADDON_CONFIG_FILE}")
 
 # set options
-MQTT="--quiet -l -i $(hostname) -h ${MQTT_HOST} -p ${MQTT_PORT} -t ${MQTT_TOPIC}"
+MQTT='-i "'${MQTT_IDENTITY}'" -h "'${MQTT_HOST}'" -p ${MQTT_PORT} -t "'${MQTT_TOPIC}"'"'
 
 # check credentials
 MQTT_USERNAME=$(jq -r ".mqtt.username" "${ADDON_CONFIG_FILE}")
@@ -20,12 +26,7 @@ if [ -n "${MQTT_USERNAME}" ] && [ -n "${MQTT_PASSWORD}" ]; then
   MQTT="${MQTT} -u ${MQTT_USERNAME} -P ${MQTT_PASSWORD}"
 fi
 
-MQTT_IDENTITY=$(jq -r ".horizon.device" "${ADDON_CONFIG_FILE}")
-if [ -z "${MQTT_IDENTITY}" ]; then
-  MQTT_IDENTITY=$(hostname)
-fi
-
-OUT='{"host":"'${MQTT_HOST}'","port":'${MQTT_PORT}',"topic":"'${MQTT_TOPIC}'","username":"'${MQTT_USERNAME}'","password":"'${MQTT_PASSWORD}',"identity":"'${MQTT_IDENTITY}'"}'
+OUT='{"host":"'"${MQTT_HOST}"","port":'${MQTT_PORT}',"topic":"'"${MQTT_TOPIC}"'","username":"'"${MQTT_USERNAME}"'","password":"'"${MQTT_PASSWORD}"'","identity":"'"${MQTT_IDENTITY}"'"}'
 
 if read -r; then
   if [ -n "${REPLY}" ]; then
@@ -38,7 +39,6 @@ else
   SIZE=-1
 fi
 
-OUT=$(echo "${OUT}" | jq '.size='${SIZE})
+echo "${OUT}" | jq '.size='${SIZE}
 
-echo "${OUT}"
 exit 0
