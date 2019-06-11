7ea0a7376bbd3c5646bf89d7ce85726d659f1285
dcmartin <github@dcmartin.com>
2019-06-10 18:19:13 -0700

    dev

--- kafka2mqtt4yolo/rootfs/usr/bin/run.sh
+++ kafka2mqtt4yolo/rootfs/usr/bin/run.sh
@@ -238,7 +238,9 @@ kafka2mqtt_process_yolo2msghub()
 
     NODE_ENTITY_COUNT=$((NODE_ENTITY_COUNT+1))
     THIS=$(echo "${THIS}" | jq '.count='${NODE_ENTITY_COUNT})
+    hass.log.debug "Setting DEVICES"
     DEVICES=$(echo "${DEVICES:-[]}" | jq '(.[]|select(.id=="'${ID}'"))|='"${THIS}")
+    hass.log.debug "Set DEVICES"
 
     # send JSON update
     TEMP=$(mktemp) && echo "${DEVICES}" | jq -c '{"'${KAFKA_TOPIC}'":{"date":"'$(date -u +%FT%TZ)'","activity":.}}' > ${TEMP}
