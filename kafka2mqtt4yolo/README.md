05a3e063b3bdff08332377b971dc298051605d8e
dcmartin <github@dcmartin.com>
2019-10-29 13:40:02 -0700

    lgtm

---
+++ kafka2mqtt4yolo/README.md
@@ -0,0 +1,109 @@
+# `kafka2mqtt4yolo` -  addon for Home Assistant
+
+
+This [Home Assistant][home-assistant] add-on is for use with
+the [Open Horizon][open-horizon] pattern-service: [`yolo2msghub`](http://github.com/dcmartin/open-horizon/master/tree/yolo2msghub/README.md)
+
+## About
+
+This addon is designed to consume Kafka messages on the topic `yolo2msghub` and produce MQTT messages for consumption by the [`MQTT broker`][core-mosquitto] addon using the specified topic (see **Options** below).
+
+Detailed [documentation][edge-fabric] for the IBM Edge Fabric is available on-line.  A Slack [channel][edge-slack] is also available.
+
+## Installation
+
+### Install addon
+
+1. [Add this repository][repository] to your Hass.io addon store.
+1. Install the "yolo2msghub" add-on
+1. Setup the "yolo2msghub" add-on
+  - Configure `kafka` with your Kafka API key
+  - Optionally change `mqtt` for `host`, `port`, `topic`, `username`, and `password`
+1. Start the "yolo2msghub" add-on
+1. Check the logs of the add-on for failures :-(
+
+## Configuration
+
+### Option: `kafka`
+Credentials required for sending and receiving messages using the IBM Cloud MessageHub.
+```
+  "kafka": {
+    "api_key": "<Your IBM Edge Fabric MessageHub API key>",
+    "brokers": "kafka03-prod02.messagehub.services.us-south.bluemix.net:9093,kafka04-prod02.messagehub.services.us-south.bluemix.net:9093,kafka01-prod02.messagehub.services.us-south.bluemix.net:9093,kafka02-prod02.messagehub.services.us-south.bluemix.net:9093,kafka05-prod02.messagehub.services.us-south.bluemix.net:9093"
+  }
+```
+
+### Option: `mqtt`
+This option provides the information required for MQTT service; defaults are as specified below.
+```
+  "mqtt": {
+    "host": "core-mosquitto",
+    "port": 1883,
+    "topic": "kafka/cpu-load"
+    "username": "",
+    "password": ""
+  }
+```
+
+## USE
+
+Listen to the MQTT host and port to receive JSON payload of the processed YOLO image.
+
+1. `kafka/yolo-image`
+
+Configuration examples are provided for processing the JSON:
+
+1. Edit your HomeAssistant configuration YAML to include [yolo2msghub][yolo-yaml]
+1. Edit your `ui-lovelace.yaml` configuration for [display][yolo-lovelace]
+
+# Sample output
+
+![sample activity](samples/sample.png?raw=true "SAMPLE")
+
+## Changelog & Releases
+
+Releases are based on Semantic Versioning, and use the format
+of ``MAJOR.MINOR.PATCH``. In a nutshell, the version will be incremented
+based on the following:
+
+- ``MAJOR``: Incompatible or major changes.
+- ``MINOR``: Backwards-compatible new features and enhancements.
+- ``PATCH``: Backwards-compatible bugfixes and package updates.
+
+## Authors & contributors
+
+David C Martin (github@dcmartin.com)
+
+[yolo-lovelace]: https://raw.githubusercontent.com/dcmartin/hassio-addons/master/yolo2msghub/ui-lovelace.yaml
+[yolo-yaml]: https://raw.githubusercontent.com/dcmartin/hassio-addons/master/yolo2msghub/yolo2msghub.yaml
+
+[commits]: https://github.com/dcmartin/hassio-addons/yolo2msghub/commits/master
+[contributors]: https://github.com/dcmartin/hassio-addons/yolo2msghub/graphs/contributors
+[releases]: https://github.com/dcmartin/hassio-addons/yolo2msghub/releases
+[issue]: https://github.com/dcmartin/hassio-addons/yolo2msghub/issues
+
+[core-mosquitto]: https://github.com/hassio-addons/repository/tree/master/mqtt
+
+[dcmartin]: https://github.com/dcmartin
+[repository]: https://github.com/dcmartin/hassio-addons
+[dcm-oh]: https://github.com/dcmartin/open-horizon
+[dcm-oh-setup]: https://github.com/dcmartin/open-horizon/tree/master/setup
+[cpu-pattern]: https://github.com/open-horizon/examples/tree/master/edge/msghub/cpu2msghub
+[yolo-pattern]: https://github.com/open-horizon/examples/tree/master/edge/msghub/yolo2msghub
+[cpu-addon]: https://github.com/dcmartin/hassio-addons/tree/master/cpu2msghub
+[yolo-addon]: https://github.com/dcmartin/hassio-addons/tree/master/yolo2msghub
+[horizon-addon]: https://github.com/dcmartin/hassio-addons/tree/master/horizon
+
+[edge-fabric]: https://console.test.cloud.ibm.com/docs/services/edge-fabric/getting-started.html
+[edge-install]: https://console.test.cloud.ibm.com/docs/services/edge-fabric/adding-devices.html
+[edge-slack]: https://ibm-appsci.slack.com/messages/edge-fabric-users/
+[home-assistant]: https://home-assistant.io/
+[hzn-setup]: https://raw.githubusercontent.com/dcmartin/hassio-addons/master/horizon/hzn-setup.sh
+[ibm-apikeys]: https://console.bluemix.net/iam/#/apikeys
+[ibm-registration]: https://console.bluemix.net/registration/
+[keepchangelog]: http://keepachangelog.com/en/1.0.0/
+[macos-install]: https://github.com/open-horizon/anax/releases
+[open-horizon]: https://github.com/open-horizon
+[repository]: https://github.com/dcmartin/hassio-addons
+[watson-nlu]: https://console.bluemix.net/catalog/services/natural-language-understanding
+[watson-stt]: https://console.bluemix.net/catalog/services/speech-to-text
---
+++ kafka2mqtt4yolo/rootfs/usr/bin/run.sh
@@ -0,0 +1,313 @@
+#!/usr/bin/with-contenv bash
+# ==============================================================================
+# set -o nounset  # Exit script on use of an undefined variable
+# set -o pipefail # Return exit status of the last command in the pipe that failed
+# set -o errexit  # Exit script when a command exits with non-zero status
+# set -o errtrace # Exit on error inside any functions or sub-shells
+
+# shellcheck disable=SC1091
+source /usr/lib/hassio-addons/base.sh
+
+# should be from environment
+DATA_DIR="/data"
+
+kafka2mqtt_config()
+{
+  hass.log.trace "${FUNCNAME[0]}"
+
+  # START ADDON_CONFIG
+  ADDON_CONFIG='{"hostname":"'"$(hostname)"'","arch":"'"$(arch)"'","date":'$(date +%s)
+  # time zone
+  VALUE=$(hass.config.get "timezone")
+  # Set the correct timezone
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE="GMT"; fi
+  hass.log.debug "Setting TIMEZONE ${VALUE}" >&2
+  cp /usr/share/zoneinfo/${VALUE} /etc/localtime
+  ADDON_CONFIG="${ADDON_CONFIG}"',"timezone":"'"${VALUE}"'"'
+
+  ## KAFKA OPTIONS
+  ADDON_CONFIG="${ADDON_CONFIG}"',"kafka":{'
+  # TOPIC
+  VALUE=$(hass.config.get "kafka.topic")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE="yolo2msghub"; fi
+  hass.log.debug "Kafka topic: ${VALUE}"
+  # first; no comma
+  ADDON_CONFIG="${ADDON_CONFIG}"'"topic":"'"${VALUE}"'"'
+  # BROKERS
+  VALUE=$(hass.config.get "kafka.broker")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then hass.log.fatal "No kafka.broker"; hass.die; fi
+  hass.log.debug "Kafka broker: ${VALUE}"
+  ADDON_CONFIG="${ADDON_CONFIG}"',"broker":"'"${VALUE}"'"'
+  # APIKEY
+  VALUE=$(hass.config.get "kafka.apikey")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then hass.log.fatal "No kafka.apikey"; hass.die; fi
+  hass.log.debug "Kafka API key: ${VALUE}"
+  ADDON_CONFIG="${ADDON_CONFIG}"',"apikey":"'"${VALUE}"'"'
+  # DONE w/ kafka
+  ADDON_CONFIG="${ADDON_CONFIG}"'}'
+
+  ## MQTT OPTIONS
+  ADDON_CONFIG="${ADDON_CONFIG}"',"mqtt":{'
+  # topic
+  VALUE=$(hass.config.get "mqtt.topic")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE="yolo2msghub"; fi
+  hass.log.debug "MQTT topic: ${VALUE}"
+  # first, no comma
+  ADDON_CONFIG="${ADDON_CONFIG}"'"topic":"'"${VALUE}"'"'
+  # host
+  VALUE=$(hass.config.get "mqtt.host")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE="core-mosquitto"; fi
+  hass.log.debug "MQTT host: ${VALUE}"
+  ADDON_CONFIG="${ADDON_CONFIG}"',"host":"'"${VALUE}"'"'
+  # port
+  VALUE=$(hass.config.get "mqtt.port")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE="1883"; fi
+  hass.log.debug "MQTT port: ${VALUE}"
+  ADDON_CONFIG="${ADDON_CONFIG}"',"port":'"${VALUE}"
+  # username
+  VALUE=$(hass.config.get "mqtt.username")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE=""; fi
+  hass.log.debug "MQTT username: ${VALUE}"
+  ADDON_CONFIG="${ADDON_CONFIG}"',"username":"'"${VALUE}"'"'
+  # password
+  VALUE=$(hass.config.get "mqtt.password")
+  if [ -z "${VALUE}" ] || [ "${VALUE}" == "null" ]; then VALUE=""; fi
+  hass.log.debug "MQTT password: ${VALUE}"
+  ADDON_CONFIG="${ADDON_CONFIG}"',"password":"'"${VALUE}"'"'
+  # DONE w/ mqtt
+  ADDON_CONFIG="${ADDON_CONFIG}"'}'
+
+  ## DONE w/ ADDON_CONFIG
+  ADDON_CONFIG="${ADDON_CONFIG}"'}'
+
+  ## configuration complete
+  hass.log.trace "CONFIGURATION complete:" $(echo "${ADDON_CONFIG}" | jq -c '.')
+  export ADDON_CONFIG_FILE="${DATA_DIR}/$(hostname)-config.json"
+  # check it
+  echo "${ADDON_CONFIG}" | jq '.' > "${ADDON_CONFIG_FILE}"
+  if [ ! -s "${ADDON_CONFIG_FILE}" ]; then
+    hass.log.fatal "Invalid addon configuration: ${ADDON_CONFIG}"
+    hass.die
+  else
+    hass.log.info "Valid addon configuration: ${ADDON_CONFIG_FILE}"
+  fi
+}
+
+mqtt_pub()
+{
+  hass.log.trace "${FUNCNAME[0]}"
+
+  if [ -z "${MQTT_DEVICE:-}" ]; then MQTT_DEVICE=$(hostname) && hass.log.warning "MQTT_DEVICE unspecified; using hostname: ${MQTT_DEVICE}"; fi
+  ARGS=${*}
+  if [ ! -z "${ARGS}" ]; then
+    hass.log.trace "got arguments: ${ARGS}"
+    if [ ! -z "${MQTT_USERNAME}" ]; then
+      ARGS='-u '"${MQTT_USERNAME}"' '"${ARGS}"
+      hass.log.trace "set username: ${ARGS}"
+    fi
+    if [ ! -z "${MQTT_PASSWORD}" ]; then
+      ARGS='-P '"${MQTT_PASSWORD}"' '"${ARGS}"
+      hass.log.trace "set password: ${ARGS}"
+    fi
+    hass.log.debug "publishing as ${MQTT_DEVICE} to ${MQTT_HOST} port ${MQTT_PORT} using arguments: ${ARGS}"
+    mosquitto_pub -i "${MQTT_DEVICE}" -h "${MQTT_HOST}" -p "${MQTT_PORT}" ${ARGS}
+  else
+    hass.log.warning "nothing to send"
+  fi
+}
+
+kafka2mqtt_process_yolo2msghub()
+{
+  hass.log.trace "${FUNCNAME[0]}"
+
+  NOW=$(date +%s)
+  DEVICES="${*}"
+  BUFFER=$(cat)
+
+  if [ ! -z "${BUFFER}" ]; then
+    PAYLOAD=$(mktemp)
+    echo "${BUFFER}"  > ${PAYLOAD}
+
+    BYTES=$(wc -c ${PAYLOAD} | awk '{ print $1 }')
+    TOTAL_BYTES=$((TOTAL_BYTES+BYTES))
+    ELAPSED=$((NOW-BEGIN))
+
+    if [ ${ELAPSED} -ne 0 ]; then BPS=$(echo "${TOTAL_BYTES} / ${ELAPSED}" | bc -l); else BPS=1; fi
+    hass.log.debug "DATA $0 $$ -- received at: $(date +%T); bytes: ${BYTES}; total bytes: ${TOTAL_BYTES}; bytes/sec: ${BPS}"
+
+    # get payload specifics
+    ID=$(jq -r '.hzn.device_id' ${PAYLOAD})
+    ENTITY=$(jq -r '.yolo2msghub.yolo.detected[]?.entity' ${PAYLOAD})
+    DATE=$(jq -r '.date' ${PAYLOAD})
+    STARTED=$((NOW-DATE))
+
+    # HZN
+    if [ $(jq '.hzn?!=null' ${PAYLOAD}) = true ]; then HZN=$(jq '.hzn' ${PAYLOAD}) HZN_STATUS=$(echo "${HZN}" | jq -c '.'); fi
+    HZN_STATUS=${HZN_STATUS:-null}
+    # WAN
+    if [ $(jq '.wan?!=null' ${PAYLOAD}) = true ]; then WAN=$(jq '.wan' ${PAYLOAD}) WAN_DOWNLOAD=$(echo "${WAN}" | jq -r '.speedtest.download'); fi
+    WAN_DOWNLOAD=${WAN_DOWNLOAD:-0}
+    # CPU
+    if [ $(jq '.cpu?!=null' ${PAYLOAD}) = true ]; then CPU=$(jq '.cpu' ${PAYLOAD}) CPU_PERCENT=$(echo "${CPU}" | jq -r '.percent'); fi
+    CPU_PERCENT=${CPU_PERCENT:-0}
+    # HAL
+    if [ $(jq '.hal?!=null' ${PAYLOAD}) = true ]; then HAL=$(jq '.hal' ${PAYLOAD}) HAL_PRODUCT=$(echo "${HAL}" | jq -r '.lshw.product'); fi
+    HAL_PRODUCT="${HAL_PRODUCT:-unknown}"
+
+    hass.log.debug "device: ${ID}; hzn: ${HZN_STATUS}; entity: ${ENTITY:-}; started: ${STARTED}; download: ${WAN_DOWNLOAD}; percent: ${CPU_PERCENT}; product: ${HAL_PRODUCT}"
+
+    # have we seen this before
+    if [ ! -z "${ID:-}" ] && [ ! -z "${DEVICES:-}" ] && [ "${DEVICES}" != '[]' ]; then
+      THIS=$(echo "${DEVICES}" | jq '.[]|select(.id=="'${ID}'")')
+    else
+      THIS='null'
+    fi
+
+    if [ -z "${THIS}" ] || [ "${THIS}" = 'null' ]; then
+      NODE_MESSAGE_COUNT=0
+      NODE_SEEN_COUNT=0
+      NODE_MOCK_COUNT=0
+      NODE_FIRST_SEEN=0
+      NODE_LAST_SEEN=0
+      NODE_AVERAGE=0
+      THIS='{"id":"'${ID:-}'","entity":"'${ENTITY}'","timestamp":"'$(date -u +%FT%TZ)'","date":'${DATE}',"started":'${STARTED}',"count":'${NODE_MESSAGE_COUNT}',"mock":'${NODE_MOCK_COUNT}',"seen":0,"total":'${NODE_SEEN_COUNT}',"first":'${NODE_FIRST_SEEN}',"last":'${NODE_LAST_SEEN}',"average":'${NODE_AVERAGE:-0}',"download":'${WAN_DOWNLOAD:-0}',"percent":'${CPU_PERCENT:-0}',"product":"'${HAL_PRODUCT:-unknown}'"}'
+    else
+      NODE_MESSAGE_COUNT=$(echo "${THIS}" | jq '.count') || NODE_MESSAGE_COUNT=0
+      NODE_MOCK_COUNT=$(echo "${THIS}" | jq '.mock') || NODE_MOCK_COUNT=0
+      NODE_SEEN_COUNT=$(echo "${THIS}" | jq '.total') || NODE_SEEN_COUNT=0
+      NODE_FIRST_SEEN=$(echo "${THIS}" | jq '.first') || NODE_FIRST_SEEN=0
+      NODE_LAST_SEEN=$(echo "${THIS}" | jq '.last') || NODE_LAST_SEEN=0
+      NODE_AVERAGE=$(echo "${THIS}" | jq '.average') || NODE_AVERAGE=0
+    fi
+
+    # test payload
+    if [ $(jq '.yolo2msghub.yolo!=null' ${PAYLOAD}) = true ]; then
+
+	    # retrieve image and convert from BASE64 to JPEG; publish image
+	    TEMP=$(mktemp)
+	    jq -r '.yolo2msghub.yolo.image' ${PAYLOAD} | base64 --decode > ${TEMP}
+	    hass.log.debug "sending image to topic ${MQTT_TOPIC}/image"
+	    mqtt_pub -t "${MQTT_TOPIC}/image" -f ${TEMP}
+	    rm -f ${TEMP}
+
+      WHEN=$(jq -r '.yolo2msghub.yolo.date' ${PAYLOAD})
+      TIMESTAMP=$(jq -r '.yolo2msghub.yolo.timestamp' ${PAYLOAD})
+      if [ $(jq -r '.yolo2msghub.yolo.mock' ${PAYLOAD}) = 'null' ]; then
+	if [ ${WHEN:-0} -gt ${NODE_LAST_SEEN} ]; then
+	  SEEN=$(jq -r '.yolo2msghub.yolo.count' ${PAYLOAD})
+	  if [ ${SEEN} -gt 0 ]; then
+	    WHENSTAMP=${TIMESTAMP:-$(date -u +%FT%TZ)}
+	    NODE_SEEN_COUNT=$((NODE_SEEN_COUNT+SEEN))
+	    if [ ${NODE_LAST_SEEN:-0} > 0 ]; then AGO=$((WHEN-NODE_LAST_SEEN)); else AGO=0; fi
+	    NODE_LAST_SEEN=${WHEN}
+	    # calculate interval
+	    if [ "${NODE_FIRST_SEEN:-0}" -eq 0 ]; then NODE_FIRST_SEEN=${NODE_LAST_SEEN}; fi
+	    INTERVAL=$((NODE_LAST_SEEN-NODE_FIRST_SEEN))
+	    if [ ${INTERVAL} -eq 0 ]; then 
+	      NODE_AVERAGE=${SEEN}
+	    else
+	      NODE_AVERAGE=$(echo "${NODE_SEEN_COUNT}/${INTERVAL}" | bc -l)
+	    fi
+	    THIS=$(echo "${THIS}" | jq '.when="'${WHENSTAMP:-}'"|.date='${WHEN}'|.interval='${INTERVAL:-0}'|.ago='${AGO:-0}'|.seen='${SEEN:-0}'|.last='${NODE_LAST_SEEN:-0}'|.first='${NODE_FIRST_SEEN:-0}'|.total='${NODE_SEEN_COUNT}'|.average='${NODE_AVERAGE:-0})
+	  else
+	    hass.log.trace "${ID} at ${WHEN:-0}; did not see: ${ENTITY:-null}"
+	  fi
+	else
+	  hass.log.trace "old payload"
+	fi
+      else
+	hass.log.warning "${ID} at ${WHEN:-0}: mock" $(jq -c '.yolo2msghub.yolo.detected' ${PAYLOAD})
+	NODE_MOCK_COUNT=$((NODE_MOCK_COUNT+1)) && THIS=$(echo "${THIS}" | jq '.mock='${NODE_MOCK_COUNT})
+      fi
+    else
+      hass.log.warning "${ID} at ${WHEN:-0}: no yolo output"
+    fi
+
+    # send copy of payload
+    hass.log.debug "sending payload to topic ${MQTT_TOPIC}/payload"
+    mqtt_pub -t "${MQTT_TOPIC}/payload" -f ${PAYLOAD}
+
+    # remove payload
+    rm -f ${PAYLOAD}
+
+    # update timestamp
+    THIS=$(echo "${THIS}" | jq '.timestamp="'$(date -u +%FT%TZ)'"')
+
+    NODE_MESSAGE_COUNT=$((NODE_MESSAGE_COUNT+1))
+    THIS=$(echo "${THIS}" | jq '.count='${NODE_MESSAGE_COUNT})
+  else
+    hass.log.trace  "received null payload:" $(date +%T)
+    THIS=
+  fi
+  echo "${THIS:-}"
+}
+
+kafka2mqtt_poll()
+{
+  hass.log.trace "${FUNCNAME[0]}"
+
+  hass.log.debug "listening: ${KAFKA_TOPIC}; ${KAFKA_APIKEY}; ${KAFKA_BROKER_URL}"
+
+  # globals
+  DEVICES=
+  TOTAL_BYTES=0
+  BEGIN=$(date +%s)
+
+  TEMP=$(mktemp)
+  kafkacat -E -u -C -q -o end -f "%s\n" -b "${KAFKA_BROKER_URL}" \
+    -X "security.protocol=sasl_ssl" \
+    -X "sasl.mechanisms=PLAIN" \
+    -X "sasl.username=${KAFKA_APIKEY:0:16}" \
+    -X "sasl.password=${KAFKA_APIKEY:16}" \
+    -t "${KAFKA_TOPIC}" | while read -r; do
+
+      THIS=$(echo "${REPLY}" | kafka2mqtt_process_yolo2msghub "${DEVICES}")
+
+      # check for null payload
+      if [ ! -z "${THIS}" ]; then
+        # check for existing devices
+        if [ ! -z "${DEVICES}" ]; then
+          ID=$(echo "${THIS}" | jq -r '.id')
+          THAT=$(echo "${DEVICES}" | jq '.[]|select(.id=="'${ID}'")')
+          if [ ! -z "${THAT}" ]; then
+	    hass.log.debug "updating device ${ID}"
+            DEVICES=$(echo "${DEVICES}" | jq '(.[]|select(.id=="'${ID}'"))|='"${THIS}")
+          else
+	    hass.log.debug "adding device ${ID}"
+            DEVICES=$(echo "${DEVICES}" | jq '.+=['"${THIS}"']')
+          fi
+        else
+	  hass.log.debug "no existing devices"
+          DEVICES='['"${THIS}"']'
+        fi 
+
+        # send JSON update
+        echo "${DEVICES}" | jq -c '{"'${KAFKA_TOPIC}'":{"date":"'$(date +%s)'","timestamp":"'$(date -u +%FT%TZ)'","activity":.|sort_by(.timestamp|strptime("%FT%TZ")|todate)}}' > ${TEMP}
+        # send summary data
+        mqtt_pub -t ${MQTT_TOPIC} -f ${TEMP}
+      fi
+  done
+}
+  
+###
+### MAIN
+###
+
+# configure
+kafka2mqtt_config
+
+# get config
+KAFKA_TOPIC=$(jq -r '.kafka.topic' "${ADDON_CONFIG_FILE}")
+KAFKA_BROKER_URL=$(jq -r '.kafka.broker' "${ADDON_CONFIG_FILE}")
+KAFKA_APIKEY=$(jq -r '.kafka.apikey' "${ADDON_CONFIG_FILE}")
+MQTT_TOPIC=$(jq -r '.mqtt.topic' "${ADDON_CONFIG_FILE}")
+MQTT_HOST=$(jq -r '.mqtt.host' "${ADDON_CONFIG_FILE}")
+MQTT_PORT=$(jq -r '.mqtt.port' "${ADDON_CONFIG_FILE}")
+MQTT_USERNAME=$(jq -r '.mqtt.username' "${ADDON_CONFIG_FILE}")
+MQTT_PASSWORD=$(jq -r '.mqtt.password' "${ADDON_CONFIG_FILE}")
+
+# forever poll
+while true; do
+  # run
+  kafka2mqtt_poll
+done
