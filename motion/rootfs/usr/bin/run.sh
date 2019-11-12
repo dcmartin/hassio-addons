ee09e9aa6b4b660a1d65a74da3f0ba862ef559d8
dcmartin <github@dcmartin.com>
2019-11-11 19:43:49 -0800

    lgtm

--- motion/rootfs/usr/bin/run.sh
+++ motion/rootfs/usr/bin/run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bash
+#!/bin/bash
 # ==============================================================================
 set -o pipefail # Return exit status of the last command in the pipe that failed
 set -o nounset  # Exit script on use of an undefined variable
@@ -398,6 +398,7 @@ hzn.log.notice "*** Found ${ncamera} cameras"
 
 MOTION_COUNT=1
 
+hzn.log.trace "*** Processing ${ncamera} cameras"
 for (( i=0; i<ncamera ; i++)) ; do
   hzn.log.trace "+++ CAMERA ${i}"
 
