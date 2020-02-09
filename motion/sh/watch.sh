bf67d693180641ccdf358a91896f7bc29aa40a53
dcmartin <github@dcmartin.com>
2020-02-09 15:06:28 -0800

    lgtm

--- motion/sh/watch.sh
+++ motion/sh/watch.sh
@@ -7,6 +7,6 @@ fi
 
 mosquitto_sub -h 192.168.1.80 -u username -P password -t "+/${HOST_NAME}/start" | jq -c '{"DEVICE":.}' &
 mosquitto_sub -h 192.168.1.80 -u username -P password -t "+/${HOST_NAME}/+/event/start" | jq -c '{"START":.}' &
-mosquitto_sub -h 192.168.1.80 -u username -P password -t "+/${HOST_NAME}/+/event/start" | jq -c '{"END":.}' &
+mosquitto_sub -h 192.168.1.80 -u username -P password -t "+/${HOST_NAME}/+/event/end" | jq -c '{"END":.}' &
 mosquitto_sub -h 192.168.1.80 -u username -P password -t "+/${HOST_NAME}/+/event/end/+" | jq -c '{"ANNOTATED":{"camera":.event.camera,"event":.event.id,"timestamp":.event.timestamp,"detected":.detected}}' &
 mosquitto_sub -h 192.168.1.80 -u username -P password -t "+/${HOST_NAME}/+/status/+" | jq -c '{"STATUS":.}' &
