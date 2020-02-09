34312318098f54a3fef996cbb73be14270f5433a
dcmartin <github@dcmartin.com>
2020-02-09 15:04:27 -0800

    lgtm

--- motion/sh/watch.sh
+++ motion/sh/watch.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
-mosquitto_sub -h 192.168.1.80 -u username -P password -t '+/pi3-3/start' | jq -c '{"DEVICE":.}' &
-mosquitto_sub -h 192.168.1.80 -u username -P password -t '+/pi3-3/+/event/start' | jq -c '{"START":.}' &
-mosquitto_sub -h 192.168.1.80 -u username -P password -t '+/pi3-3/+/event/start' | jq -c '{"END":.}' &
-mosquitto_sub -h 192.168.1.80 -u username -P password -t '+/pi3-3/+/event/end/+' | jq -c '{"ANNOTATED":{"camera":.event.camera,"event":.event.id,"timestamp":.event.timestamp,"detected":.detected}}' &
-mosquitto_sub -h 192.168.1.80 -u username -P password -t '+/pi3-3/+/status/+' | jq -c '{"STATUS":.}' &
+#!/bin/bash -x
+if [ -s "HOST_NAME" ]; then 
+  HOST_NAME=$(cat "HOST_NAME")
+else 
+  HOST_NAME=$(hostname)
+fi
+
+mosquitto_sub -h 192.168.1.80 -u username -P password -t "+/${HOST_NAME}/start" | jq -c '{"DEVICE":.}' &
+mosquitto_sub -h 192.168.1.80 -u username -P password -t "+/${HOST_NAME}/+/event/start" | jq -c '{"START":.}' &
+mosquitto_sub -h 192.168.1.80 -u username -P password -t "+/${HOST_NAME}/+/event/start" | jq -c '{"END":.}' &
+mosquitto_sub -h 192.168.1.80 -u username -P password -t "+/${HOST_NAME}/+/event/end/+" | jq -c '{"ANNOTATED":{"camera":.event.camera,"event":.event.id,"timestamp":.event.timestamp,"detected":.detected}}' &
+mosquitto_sub -h 192.168.1.80 -u username -P password -t "+/${HOST_NAME}/+/status/+" | jq -c '{"STATUS":.}' &
